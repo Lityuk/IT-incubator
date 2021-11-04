32a90181b9259db5ae49221eb5a40cbda71ac431
Lityuk <lityuk@gmail.com>
2021-11-04 23:14:59 +0300

    feat: basick JS 51-55 tasks

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -157,86 +157,161 @@
 // const lastName = "Lovelace";
 // const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
 
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
 
-const wordBlanks =
-  "The " +
-  myNoun +
-  " is " +
-  myAdjective +
-  " and he " +
-  myVerb +
-  " very " +
-  myAdverb;
+// const wordBlanks =
+//   "The " +
+//   myNoun +
+//   " is " +
+//   myAdjective +
+//   " and he " +
+//   myVerb +
+//   " very " +
+//   myAdverb;
 
-const myArray = ["string", 3];
+// const myArray = ["string", 3];
 
-// Only change code below this line
-const myArray = [[], []];
+// // Only change code below this line
+// const myArray = [[], []];
 
-//!! 41 -  Tasks
-const myArray = [50, 60, 70];
+// //!! 41-50 -  Tasks
+// const myArray = [50, 60, 70];
 
-let myData = myArray[0];
+// let myData = myArray[0];
 
-// Setup
-const myArray = [18, 64, 99];
+// // Setup
+// const myArray = [18, 64, 99];
 
-// Only change code below this line
-myArray[0] = 45;
+// // Only change code below this line
+// myArray[0] = 45;
 
-const myArray = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [[10, 11, 12], 13, 14],
-];
+// const myArray = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+//   [[10, 11, 12], 13, 14],
+// ];
 
-const myData = myArray[2][1];
+// const myData = myArray[2][1];
 
-// Setup
-const myArray = [
-  ["John", 23],
-  ["cat", 2],
-];
+// // Setup
+// const myArray = [
+//   ["John", 23],
+//   ["cat", 2],
+// ];
 
-// Only change code below this line
-myArray.push(["dog", 3]);
+// // Only change code below this line
+// myArray.push(["dog", 3]);
 
-// Setup
-const myArray = [
-  ["John", 23],
-  ["cat", 2],
-];
+// // Setup
+// const myArray = [
+//   ["John", 23],
+//   ["cat", 2],
+// ];
 
-// Only change code below this line
-let removedFromMyArray = myArray.pop();
+// // Only change code below this line
+// let removedFromMyArray = myArray.pop();
 
-let removedFromMyArray = myArray.shift();
+// let removedFromMyArray = myArray.shift();
 
-// Setup
-const myArray = [
-  ["John", 23],
-  ["dog", 3],
-];
-myArray.shift();
+// // Setup
+// const myArray = [
+//   ["John", 23],
+//   ["dog", 3],
+// ];
+// myArray.shift();
 
-// Only change code below this line
-myArray.unshift(["Paul", 35]);
+// // Only change code below this line
+// myArray.unshift(["Paul", 35]);
 
-const myList = [
-  ["Chocolate Bar", 15],
-  ["Bar2", 16],
-  ["Bar3", 17],
-  ["Bar4", 20],
-  ["Bar5", 25],
-];
+// const myList = [
+//   ["Chocolate Bar", 15],
+//   ["Bar2", 16],
+//   ["Bar3", 17],
+//   ["Bar4", 20],
+//   ["Bar5", 25],
+// ];
 
-function reusableFunction() {
-    console.log("Hi World");
+// function reusableFunction() {
+//     console.log("Hi World");
+//   }
+
+//   reusableFunction();
+
+//   function functionWithArgs(a,b) {
+//     console.log(a + b);
+//     }
+
+//     functionWithArgs(3, 5);
+
+//!! 51-60 -  Tasks
+
+function timesFive(num) {
+    return num * 5;
   }
 
-  reusableFunction();
+  // Declare the myGlobal variable below this line
+let myGlobal = 10;
+let oopsGlobal;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+function myLocalScope() {
+    // Only change code below this line
+  var myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+
+  // Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+let outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+

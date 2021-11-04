99b870a548348fb77771cc4685f80a53bd04e3a2
Lityuk <lityuk@gmail.com>
2021-11-05 00:00:27 +0300

    feat: basick JS 63 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -20,3 +20,14 @@ function compareEquality(a, b) {
   }
 
   compareEquality(10, "10");
+
+  //!! 63 -  Task
+  // Setup
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+
+  testNotEqual(10);

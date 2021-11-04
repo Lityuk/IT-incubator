6085daefdf326e056b29f62d7357fbba8cd580c4
Lityuk <lityuk@gmail.com>
2021-11-04 18:45:26 +0300

    feat: basick JS 41-50 tasks

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -123,7 +123,6 @@
 // const myName = "Vladimir";
 // const myStr = "My name is " + myName + " and I am well!";
 
-
 // const someAdjective = "amazing";
 // let myStr = "Learning to code is ";
 // myStr += someAdjective;
@@ -134,7 +133,6 @@
 
 // // Only change code below this line
 // lastNameLength = lastName.length;
-
 
 // // Setup
 // let firstLetterOfLastName = "";
@@ -164,8 +162,15 @@ const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
-const wordBlanks = "The " + myNoun + " is " + myAdjective + " and he " + myVerb + " very " + myAdverb;
-
+const wordBlanks =
+  "The " +
+  myNoun +
+  " is " +
+  myAdjective +
+  " and he " +
+  myVerb +
+  " very " +
+  myAdverb;
 
 const myArray = ["string", 3];
 
@@ -173,5 +178,65 @@ const myArray = ["string", 3];
 const myArray = [[], []];
 
 //!! 41 -  Tasks
+const myArray = [50, 60, 70];
 
+let myData = myArray[0];
 
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+let removedFromMyArray = myArray.pop();
+
+let removedFromMyArray = myArray.shift();
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+const myList = [
+  ["Chocolate Bar", 15],
+  ["Bar2", 16],
+  ["Bar3", 17],
+  ["Bar4", 20],
+  ["Bar5", 25],
+];
+
+function reusableFunction() {
+    console.log("Hi World");
+  }
+
+  reusableFunction();

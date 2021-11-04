4630ea813885b268393752df0257f2e47393eed9
Lityuk <lityuk@gmail.com>
2021-11-04 23:51:38 +0300

    feat: basick JS 61 task

---
+++ freecodecamp/script-2.js
@@ -0,0 +1,12 @@
+//!! 61 -  Task
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+
+  testStrict(10);
+
+  //!! 62 -  Task

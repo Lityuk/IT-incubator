a8310fce52305a3ade64cbf5d84ec5e29a4ae3c7
Lityuk <lityuk@gmail.com>
2021-11-04 23:28:00 +0300

    feat: basick JS 58 task

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -321,6 +321,35 @@ let processed = 0;
 function processArg(num) {
   return (num + 3) / 5;
 }
-//!! 56 -  Tasks
+//!! 56 -  Task
 // Only change code below this line
 processed = processArg(7);
+
+//!! 57 -  Task
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    let deleted = arr[0];
+    arr.shift();
+
+    return deleted;
+    // Only change code above this line
+  }
+
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+
+  //!! 58 -  Task
+  function welcomeToBooleans() {
+    // Only change code below this line
+
+    return true; // Change this line
+
+    // Only change code above this line
+  }

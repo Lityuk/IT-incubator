e2245ee2eb850a83a11db57532ee351a146588ed
Lityuk <lityuk@gmail.com>
2021-11-05 22:04:57 +0300

    feat: basick JS 87 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -453,3 +453,14 @@ const testObj = {
   // Only change code below this line
   const playerNumber = 16;  // Change this line
   const player = testObj[playerNumber];   // Change this line
+
+//!! 87 -  Task
+  // Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = "Happy Coder";
+  // Only change code below this line

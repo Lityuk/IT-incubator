850f149705906d5abf0336952ff019834c5e64e1
Lityuk <lityuk@gmail.com>
2021-11-05 13:31:02 +0300

    feat: basick JS 64 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -1,33 +1,44 @@
-//!! 61 -  Task
+// //!! 61 -  Task
+// // Setup
+// function testStrict(val) {
+//     if (val === 7) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+
+//   testStrict(10);
+
+//   //!! 62 -  Task
+
+//   // Setup
+// function compareEquality(a, b) {
+//     if (a === b) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+
+//   compareEquality(10, "10");
+
+//   //!! 63 -  Task
+//   // Setup
+// function testNotEqual(val) {
+//     if (val != 99) { // Change this line
+//       return "Not Equal";
+//     }
+//     return "Equal";
+//   }
+
+//   testNotEqual(10);
+
+//   //!! 64 -  Task
 // Setup
-function testStrict(val) {
-    if (val === 7) { // Change this line
-      return "Equal";
-    }
-    return "Not Equal";
-  }
-
-  testStrict(10);
-
-  //!! 62 -  Task
-
-  // Setup
-function compareEquality(a, b) {
-    if (a === b) { // Change this line
-      return "Equal";
-    }
-    return "Not Equal";
-  }
-
-  compareEquality(10, "10");
-
-  //!! 63 -  Task
-  // Setup
-function testNotEqual(val) {
-    if (val != 99) { // Change this line
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
       return "Not Equal";
     }
     return "Equal";
   }
 
-  testNotEqual(10);
+  testStrictNotEqual(10);

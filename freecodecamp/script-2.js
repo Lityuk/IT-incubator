27aaaf65dc2a515961827b47ff74405bf1fda2d6
Lityuk <lityuk@gmail.com>
2021-11-05 14:52:49 +0300

    feat: basick JS 76 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -173,61 +173,67 @@ function testElseIf(val) {
 
 testElseIf(7);
 
-
 //!! 73 -  Task
 function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
-    } else {
-      return "Greater than or equal to 10";
-    }
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
   }
+}
 
-  orderMyLogic(7);
+orderMyLogic(7);
 
 //!! 74 -  Task
-  function testSize(num) {
-    // Only change code below this line
-  if(num < 5) {
-    return "Tiny"
-  } else if(num < 10) {
-    return "Small"
-  } else if(num < 15) {
-    return "Medium"
-  } else if(num < 20) {
-    return "Large"
-  } else if (num >= 20){
-    return "Huge"
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
   }
 
-    return "Change Me";
-    // Only change code above this line
-  }
+  return "Change Me";
+  // Only change code above this line
+}
 
-  testSize(7);
-
+testSize(7);
 
 //!! 75 -  Task
-  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
 
 function golfScore(par, strokes) {
   // Only change code below this line
   if (strokes == 1) {
-    return names[0]
+    return names[0];
   } else if (strokes <= par - 2) {
-    return names[1]
+    return names[1];
   } else if (strokes == par - 1) {
-    return names[2]
+    return names[2];
   } else if (strokes == par) {
-    return names[3]
+    return names[3];
   } else if (strokes == par + 1) {
-    return names[4]
-  }else if (strokes == par + 2) {
-    return names[5]
-  }else if (strokes >= par + 3) {
-    return names[6]
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
   }
 
   return "Change Me";
@@ -235,3 +241,29 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+
+//!! 76 -  Task
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);

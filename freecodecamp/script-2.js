1959e002bf02cd35bfec1dbe191e41048e9b36a8
Lityuk <lityuk@gmail.com>
2021-11-05 13:49:44 +0300

    feat: basick JS 70 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -129,3 +129,16 @@ function testLogicalAnd(val) {
 testLogicalAnd(10);
 
 //!! 70 -  Task
+
+function testLogicalOr(val) {
+    // Only change code below this line
+
+    if (val <10 || val > 20) {
+      return "Outside";
+    }
+
+    // Only change code above this line
+    return "Inside";
+  }
+
+  testLogicalOr(15);

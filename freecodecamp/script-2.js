8d39bb2c212a9e0ad2df6c32bee8d7ca3f4cd186
Lityuk <lityuk@gmail.com>
2021-11-05 13:48:33 +0300

    feat: basick JS 69 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -35,72 +35,97 @@
 //   //!! 64 -  Task
 // Setup
 function testStrictNotEqual(val) {
-    if (val !== 17) { // Change this line
-      return "Not Equal";
-    }
-    return "Equal";
+  if (val !== 17) {
+    // Change this line
+    return "Not Equal";
   }
+  return "Equal";
+}
 
-  testStrictNotEqual(10);
+testStrictNotEqual(10);
 
 //   //!! 65 -  Task
-  function testGreaterThan(val) {
-    if (val > 100) {  // Change this line
-      return "Over 100";
-    }
-
-    if (val > 10) {  // Change this line
-      return "Over 10";
-    }
-
-    return "10 or Under";
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
   }
 
-  testGreaterThan(10);
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
 
 //   //!! 66 -  Task
-  function testGreaterOrEqual(val) {
-    if (val >= 20) {  // Change this line
-      return "20 or Over";
-    }
-
-    if (val >= 10) {  // Change this line
-      return "10 or Over";
-    }
-
-    return "Less than 10";
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    // Change this line
+    return "20 or Over";
   }
 
-  testGreaterOrEqual(10);
+  if (val >= 10) {
+    // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
 
 //   //!! 67 -  Task
-  function testLessThan(val) {
-    if (val < 25) {  // Change this line
-      return "Under 25";
-    }
-
-    if (val < 55) {  // Change this line
-      return "Under 55";
-    }
-
-    return "55 or Over";
+function testLessThan(val) {
+  if (val < 25) {
+    // Change this line
+    return "Under 25";
   }
 
-  testLessThan(10);
-
-  //!! 68 -  Task
-
-  function testLessOrEqual(val) {
-    if (val <= 12) {  // Change this line
-      return "Smaller Than or Equal to 12";
-    }
-
-    if (val <= 24) {  // Change this line
-      return "Smaller Than or Equal to 24";
-    }
-
-    return "More Than 24";
+  if (val < 55) {
+    // Change this line
+    return "Under 55";
   }
 
-  testLessOrEqual(10);
+  return "55 or Over";
+}
 
+testLessThan(10);
+
+//!! 68 -  Task
+
+function testLessOrEqual(val) {
+  if (val <= 12) {
+    // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {
+    // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+//!! 69 -  Task
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+//!! 70 -  Task

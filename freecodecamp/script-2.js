b4163236e55a1a3a4d57bd10e1680b76d99a01ed
Lityuk <lityuk@gmail.com>
2021-11-05 13:43:49 +0300

    feat: basick JS 68 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -87,3 +87,20 @@ function testStrictNotEqual(val) {
   }
 
   testLessThan(10);
+
+  //!! 68 -  Task
+
+  function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+
+    return "More Than 24";
+  }
+
+  testLessOrEqual(10);
+

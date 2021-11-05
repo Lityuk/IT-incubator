cdf6607fac5c2a80e1df855d875830a5e1e14818
Lityuk <lityuk@gmail.com>
2021-11-05 23:58:41 +0300

    feat: basick JS 95 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -487,107 +487,147 @@
 //   // Only change code below this line
 //   delete myDog.tails;
 
-
-
-
 //!! 90 -  Task
-function phoneticLookup(val) {
-  let result = "";
+// function phoneticLookup(val) {
+//   let result = "";
+
+//   // Only change code below this line
+//   let loocup = {
+//     alpha: "Adams",
+
+//     bravo: "Boston",
+
+//     charlie: "Chicago",
+
+//     delta: "Denver",
+//     echo: "Easy",
+
+//     foxtrot: "Frank",
+//   };
+
+//   result = loocup[val];
+//   // Only change code above this line
+//   return result;
+// }
+
+// phoneticLookup("charlie");
+
+// //!! 91 -  Task
+
+// function checkObj(obj, checkProp) {
+//     // Only change code below this line
+
+//     return obj.hasOwnProperty(checkProp) ? obj[checkProp]:"Not Found";
+
+//     // Only change code above this line
+//   }
+
+// //!! 92 -  Task
+
+//   const myMusic = [
+//     {
+//       "artist": "Billy Joel",
+//       "title": "Piano Man",
+//       "release_year": 1973,
+//       "formats": [
+//         "CD",
+//         "8T",
+//         "LP"
+//       ],
+//       "gold": true,
+//     },
+//     {
+//       "artist": "Nirvana",
+//       "title": "In Utero",
+//       "release_year": 1993,
+//       "formats": [
+//         "CD",
+//         "8T",
+//         "LP"
+//       ],
+//     }
+//   ];
+
+// //!! 93 -  Task
+
+//   const myStorage = {
+//     "car": {
+//       "inside": {
+//         "glove box": "maps",
+//         "passenger seat": "crumbs"
+//        },
+//       "outside": {
+//         "trunk": "jack"
+//       }
+//     }
+//   };
+
+//   const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// //!! 94 -  Task
+//   const myPlants = [
+//     {
+//       type: "flowers",
+//       list: [
+//         "rose",
+//         "tulip",
+//         "dandelion"
+//       ]
+//     },
+//     {
+//       type: "trees",
+//       list: [
+//         "fir",
+//         "pine",
+//         "birch"
+//       ]
+//     }
+//   ];
+
+//   const secondTree = myPlants[1].list[1];
+
+
+
+//!! 95 -  Task
+
+// Setup
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
 
   // Only change code below this line
-  let loocup = {
-    alpha: "Adams",
+  function updateRecords(records, id, prop, value) {
 
-    bravo: "Boston",
+    if (prop != "tracks" && value != "") {
+      records[id][prop] = value;
+    };
+    if (prop == "tracks" && records[id][prop] == undefined) {
+      records[id][prop] = [];
+      records[id][prop].push(value);
+    } else if (prop == "tracks" && value != "") {
+      records[id][prop].push(value);
+    };
+    if (value == "") {
+      delete records[id][prop];
+    }
 
-    charlie: "Chicago",
-
-    delta: "Denver",
-    echo: "Easy",
-
-    foxtrot: "Frank",
-  };
-
-  result = loocup[val];
-  // Only change code above this line
-  return result;
-}
-
-phoneticLookup("charlie");
-
-
-
-//!! 91 -  Task
-
-function checkObj(obj, checkProp) {
-    // Only change code below this line
-
-    return obj.hasOwnProperty(checkProp) ? obj[checkProp]:"Not Found";
-
-    // Only change code above this line
+    return records;
   }
 
-
-//!! 92 -  Task
-
-  const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true,
-    },
-    {
-      "artist": "Nirvana",
-      "title": "In Utero",
-      "release_year": 1993,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-    }
-  ];
-
-//!! 93 -  Task
-
-  const myStorage = {
-    "car": {
-      "inside": {
-        "glove box": "maps",
-        "passenger seat": "crumbs"
-       },
-      "outside": {
-        "trunk": "jack"
-      }
-    }
-  };
-
-  const gloveBoxContents = myStorage.car.inside["glove box"];
-
-//!! 94 -  Task
-  const myPlants = [
-    {
-      type: "flowers",
-      list: [
-        "rose",
-        "tulip",
-        "dandelion"
-      ]
-    },
-    {
-      type: "trees",
-      list: [
-        "fir",
-        "pine",
-        "birch"
-      ]
-    }
-  ];
-
-  const secondTree = myPlants[1].list[1];
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');

2e51edc6a16a2c1499594d42cc9eccd6b2182e64
Lityuk <lityuk@gmail.com>
2021-11-05 22:56:03 +0300

    feat: basick JS 91 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -514,3 +514,15 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+
+
+//!! 91 -  Task
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+
+    return obj.hasOwnProperty(checkProp) ? obj[checkProp]:"Not Found";
+
+    // Only change code above this line
+  }

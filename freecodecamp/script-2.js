79f8dfbd938a700fb10197657aeb74112adf78e4
Lityuk <lityuk@gmail.com>
2021-11-06 17:53:40 +0300

    feat: basick JS 98 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -649,3 +649,12 @@ for (let i = 1; i <= 5; i++) {
   myArray.push(i);
 }
 // Only change code below this line
+
+//!! 98 -  Task
+// Setup
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+// Only change code below this line
+

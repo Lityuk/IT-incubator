3374e04d18256ae819ed8f7c4ee9061910c4c016
Lityuk <lityuk@gmail.com>
2021-11-06 15:22:29 +0300

    feat: basick JS 96 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -586,48 +586,58 @@
 
 //   const secondTree = myPlants[1].list[1];
 
-
-
 //!! 95 -  Task
 
 // Setup
 const recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
 
-  // Only change code below this line
-  function updateRecords(records, id, prop, value) {
-
-    if (prop != "tracks" && value != "") {
-      records[id][prop] = value;
-    };
-    if (prop == "tracks" && records[id][prop] == undefined) {
-      records[id][prop] = [];
-      records[id][prop].push(value);
-    } else if (prop == "tracks" && value != "") {
-      records[id][prop].push(value);
-    };
-    if (value == "") {
-      delete records[id][prop];
-    }
-
-    return records;
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != "tracks" && value != "") {
+    records[id][prop] = value;
+  }
+  if (prop == "tracks" && records[id][prop] == undefined) {
+    records[id][prop] = [];
+    records[id][prop].push(value);
+  } else if (prop == "tracks" && value != "") {
+    records[id][prop].push(value);
+  }
+  if (value == "") {
+    delete records[id][prop];
   }
 
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+  return records;
+}
+
+updateRecords(recordCollection, 5439, "artist", "ABBA");
+
+
+//!! 96 -  Task
+
+// Setup
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+// Only change code below this line

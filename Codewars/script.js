141856044aca5656d6e0efed08c438e5f80de8fd
Lityuk <lityuk@gmail.com>
2021-11-07 20:32:17 +0300

    feat: basick JS 99 task and CDW task

--- Codewars/script.js
+++ Codewars/script.js
@@ -1,3 +1,8 @@
 var greet = function (name) {
   return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
 };
+
+
+function litres(time) {
+  return Math.floor(time/2);
+}
--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -658,3 +658,11 @@ for (let i = 1; i < 10; i += 2) {
 }
 // Only change code below this line
 
+//!! 99 -  Task
+// Setup
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line
+

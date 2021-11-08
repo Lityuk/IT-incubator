0f596c370a0c7a87886693e513d65dd96cb9b5f3
Lityuk <lityuk@gmail.com>
2021-11-08 23:19:17 +0300

    feat: basick JS 104 task

--- Codewars/script.js
+++ Codewars/script.js
@@ -7,9 +7,6 @@ function litres(time) {
 }
 
 function solution(number) {
-  if (number < 0) {
-    return 0;
-  }
   let sum = 0;
   for (let i = 0; i < number; i++) {
     if (i % 3 == 0 || i % 5 == 0) {

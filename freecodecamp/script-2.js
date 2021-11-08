4e723bdea432c27f1b214bb484eccf5ae9bc8fc0
Lityuk <lityuk@gmail.com>
2021-11-08 15:12:26 +0300

    feat: basick JS 100 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -666,3 +666,11 @@ for (let i = 9; i > 0; i -= 2) {
 }
 // Only change code below this line
 
+//!! 100 -  Task
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+}
+// Only change code below this line

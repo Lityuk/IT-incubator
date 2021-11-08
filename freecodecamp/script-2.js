0907682ed1472c9ac5ca5e605c8fb0311b409764
Lityuk <lityuk@gmail.com>
2021-11-09 00:24:59 +0300

    feat: basick JS 110 task

--- freecodecamp/script-2.js
+++ freecodecamp/script-2.js
@@ -803,3 +803,10 @@ function convertToInteger(str) {
 
   convertToInteger("10011");
 
+
+// //!! 110 -  Task
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+    }
+
+    checkEqual(1, 2);

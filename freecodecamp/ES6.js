35861ae39f0819a451549d7df664e67c375a5e7d
Lityuk <lityuk@gmail.com>
2021-11-09 17:21:14 +0300

    feat: solve ES6 2 task

--- freecodecamp/ES6.js
+++ freecodecamp/ES6.js
@@ -1,9 +1,15 @@
 function checkScope() {
-    let i = 'function scope';
-    if (true) {
-     let i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
   }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+const s = [5, 7, 2];
+function editInPlace() {
+  s.splice(0, 3, 2, 5, 7);
+}
+editInPlace();

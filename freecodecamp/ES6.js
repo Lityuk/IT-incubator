0c24ba158e1618bc659a736621243704828f541a
Lityuk <lityuk@gmail.com>
2021-11-11 00:35:50 +0300

    feat: solve ES6 12 task

--- freecodecamp/ES6.js
+++ freecodecamp/ES6.js
@@ -90,3 +90,8 @@ const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
 
 
 // Only change code above this line
+
+
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a];

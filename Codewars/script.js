558e5e5386d8009603d2ddd7bb1e5835dd34023e
Lityuk <lityuk@gmail.com>
2021-11-11 21:15:59 +0300

    init: start cdw Unlucky Days task

--- Codewars/script.js
+++ Codewars/script.js
@@ -1,55 +1,69 @@
-var greet = function (name) {
-  return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
-};
+// var greet = function (name) {
+//   return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
+// };
 
-function litres(time) {
-  return Math.floor(time / 2);
-}
+// function litres(time) {
+//   return Math.floor(time / 2);
+// }
 
-function solution(number) {
-  let sum = 0;
-  for (let i = 0; i < number; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
-    }
+// function solution(number) {
+//   let sum = 0;
+//   for (let i = 0; i < number; i++) {
+//     if (i % 3 == 0 || i % 5 == 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+
+// // TODO: implement the four functions specified.
+// function head(arr) {
+//   return arr[0];
+// }
+
+// function tail(arr) {
+//   return arr.slice(1);
+// }
+
+// function init(arr) {
+//   return arr.slice(0, arr.length - 1);
+// }
+
+// function last(arr) {
+//   return arr[arr.length - 1];
+// }
+
+// function deepCount(arr) {
+//   let sum = arr.length;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       sum += deepCount(arr[i]);
+//     }
+//   }
+//   console.log(sum);
+//   return sum;
+// }
+
+// // deepCount([1, 2, [3, 4, [5]]]);
+
+// // !The Coupon Code
+
+// function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+//   return (
+//     enteredCode === correctCode &&
+//     new Date(currentDate) <= new Date(expirationDate)
+//   );
+// }
+
+function unluckyDays(year) {
+  let count = 0;
+  for (let i = 0; i < 12; i ++) {
+
   }
-  return sum;
+  let date = new Date(year, 0, );
+  console.log(date);
+  console.log(date.getFullYear());
 }
 
-// TODO: implement the four functions specified.
-function head(arr) {
-  return arr[0];
-}
-
-function tail(arr) {
-  return arr.slice(1);
-}
-
-function init(arr) {
-  return arr.slice(0, arr.length - 1);
-}
-
-function last(arr) {
-  return arr[arr.length - 1];
-}
-
-function deepCount(arr) {
-  let sum = arr.length;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      sum += deepCount(arr[i]);
-    }
-  }
-  console.log(sum);
-  return sum;
-}
-
-deepCount([1, 2, [3, 4, [5]]]);
-
-
-// !The Coupon Code
-
-function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
-  return (enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate));
-}
+unluckyDays(2065);

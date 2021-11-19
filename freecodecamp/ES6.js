60bc1095f5f42b75feaa7c1a17b7f031fc405f04
Lityuk <lityuk@gmail.com>
2021-11-19 14:55:57 +0300

    feat: solve ES6 23 task

--- freecodecamp/ES6.js
+++ freecodecamp/ES6.js
@@ -207,3 +207,10 @@ import { uppercaseString, lowercaseString } from './string_functions.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");

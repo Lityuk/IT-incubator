b18e8be554635b32466d6209a5a269307b478e52
Lityuk <lityuk@gmail.com>
2021-11-22 01:06:40 +0300

    continue with react

--- React/01-app/my-app/src/App.js
+++ React/01-app/my-app/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
-function App() {
+// *** start
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// ***finish
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
     </div>
   );
-}
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="">Home</a>
+      <a href="">News Feed</a>
+      <a href="">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div className="App-header">
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;

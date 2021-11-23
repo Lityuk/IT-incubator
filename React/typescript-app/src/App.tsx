7101a30357d25832e54e803d156668fd2821d82f
Lityuk <lityuk@gmail.com>
2021-11-23 23:23:20 +0300

    feat: start typescript project

---
+++ React/typescript-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+};
+
+
+
+export default App;

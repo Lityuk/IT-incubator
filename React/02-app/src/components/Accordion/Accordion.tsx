9539cb08c1d314361858209dc44fc7199cbf9c5b
Lityuk <lityuk@gmail.com>
2021-11-25 23:01:08 +0300

    feat: add if condition (HW)

--- React/02-app/src/components/Accordion/Accordion.tsx
+++ React/02-app/src/components/Accordion/Accordion.tsx
@@ -1,42 +1,43 @@
 import React from "react";
 
 type AccordionPropsType = {
-    titleAccordion: string;
-}
+  titleAccordion: string;
+  collapsed: boolean;
+};
 
 function Accordion(props: AccordionPropsType) {
-
+  if (props.collapsed === true) {
     return (
-        <div>
-            <AccordionTitle title={props.titleAccordion}/>
-            <AccordionBody/>
-
-        </div>
-    )
+      <div>
+        <AccordionTitle title={props.titleAccordion} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <AccordionTitle title={props.titleAccordion} />
+        <AccordionBody />
+      </div>
+    );
+  }
 }
 
 type AccordionTitlePropsType = {
-    title: string;
-}
+  title: string;
+};
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-
-    return (
-        <h3>{props.title}</h3>
-    )
+  return <h3>{props.title}</h3>;
 }
 
-
-
 function AccordionBody() {
-
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>2</li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>2</li>
+    </ul>
+  );
 }
 
 export default Accordion;

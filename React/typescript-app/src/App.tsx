64d82712b8ef0909a552c1d3e01f2226da90df45
Lityuk <lityuk@gmail.com>
2021-11-26 11:35:11 +0300

    feat: add css

--- React/typescript-app/src/App.tsx
+++ React/typescript-app/src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import React from "react";
+import './App.css';
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            <Technologies />
-        </div>
-    );
+  return (
+    <div className='app-wrapepr'>
+      <header className='header'>
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="" />
+      </header>
+      <nav className='navBar'>
+        <ul>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+        </ul>
+      </nav>
+        <div className='content'>Main content</div>
+    </div>
+  );
 };
-
-
 
 export default App;

397768ab01b8c9ba395173c2bed413b96dcc6506
Lityuk <lityuk@gmail.com>
2021-11-29 21:40:00 +0300

    feat: solve ES6 27 task

--- freecodecamp/ES6.js
+++ freecodecamp/ES6.js
@@ -227,14 +227,28 @@
 //
 // subtract(7, 4);
 
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer represents a response from a server
+//     let responseFromServer;
+
+//     if (responseFromServer) {
+//         resolve("We got the data");
+//     } else {
+//         reject("Data not received");
+//     }
+// });
+
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer represents a response from a server
-    let responseFromServer;
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
 
     if (responseFromServer) {
-        resolve("We got the data");
+      resolve("We got the data");
     } else {
-        reject("Data not received");
+      reject("Data not received");
     }
-});
+    makeServerRequest.then(result => {
+      console.log(result);
+    });
+  });
 

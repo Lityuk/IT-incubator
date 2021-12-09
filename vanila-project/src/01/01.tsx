0ecbb6e3a0e28720ba519836f6c5eae0eb73e3f1
Lityuk <lityuk@gmail.com>
2021-12-09 23:57:09 +0300

    start ignat-dz project

--- vanila-project/src/01/01.tsx
+++
@@ -1,7 +0,0 @@
-export let sum = (a: number, b: number) => a + b;
-export let mult = (a: number, b: number) => a * b;
-
-
-export function splitIntoWords(sentence: string) {
-  return sentence.toLowerCase().split(" ").map(word=>word.replace("!",""));
-}

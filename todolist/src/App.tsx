8d4951880c3f9612e2f102c28d80e7e8bd1fb133
Lityuk <lityuk@gmail.com>
2021-12-13 17:45:50 +0300

    add prettier. Ready to lesson 2

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./App.css";
-import { Todolist } from "./Todolist";
+import {Todolist} from "./Todolist";
 
 function App() {
 
-    const task1=[
-        {id:1,title:'HTML&CSS',isDone:true},
-        {id:2,title:'JS',isDone:false},
-        {id:3,title:'React',isDone:true},
+    const task1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: false},
+        {id: 3, title: 'React', isDone: true},
     ]
 
-    const task2=[
-        {id:1,title:'HTML&CSS222222',isDone:true},
-        {id:2,title:'JS22222222222',isDone:false},
-        {id:3,title:'React22222222',isDone:true},
+    const task2 = [
+        {id: 1, title: 'HTML&CSS222222', isDone: true},
+        {id: 2, title: 'JS22222222222', isDone: false},
+        {id: 3, title: 'React22222222', isDone: true},
     ]
 
     return (
-    <div className="App">
-      <Todolist title={"What to learn"} task={task1}/>
-      <Todolist title={"What to 222222"} task={task2}/>
-    </div>
-  );
+        <div className="App">
+            <Todolist title="What to learn" task={task1}/>
+            <Todolist title="What to 222222" task={task2}/>
+        </div>
+    );
 }
 
 export default App;

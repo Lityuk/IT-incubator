33c45f7ae4c70afd72720a337afd3783382d453f
Lityuk <lityuk@gmail.com>
2021-12-14 18:34:20 +0300

    feat: solve cdw Bumps in the Road task

--- Codewars/script.js
+++ Codewars/script.js
@@ -1,48 +1,48 @@
 var greet = function (name) {
-  return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
+    return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
 };
 
 function litres(time) {
-  return Math.floor(time / 2);
+    return Math.floor(time / 2);
 }
 
 function solution(number) {
-  let sum = 0;
-  for (let i = 0; i < number; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
+    let sum = 0;
+    for (let i = 0; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
     }
-  }
-  return sum;
+    return sum;
 }
 
 // TODO: implement the four functions specified.
 function head(arr) {
-  return arr[0];
+    return arr[0];
 }
 
 function tail(arr) {
-  return arr.slice(1);
+    return arr.slice(1);
 }
 
 function init(arr) {
-  return arr.slice(0, arr.length - 1);
+    return arr.slice(0, arr.length - 1);
 }
 
 function last(arr) {
-  return arr[arr.length - 1];
+    return arr[arr.length - 1];
 }
 
 function deepCount(arr) {
-  let sum = arr.length;
+    let sum = arr.length;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      sum += deepCount(arr[i]);
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            sum += deepCount(arr[i]);
+        }
     }
-  }
-  console.log(sum);
-  return sum;
+    console.log(sum);
+    return sum;
 }
 
 // deepCount([1, 2, [3, 4, [5]]]);
@@ -50,36 +50,36 @@ function deepCount(arr) {
 // !The Coupon Code
 
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
-  return (
-    enteredCode === correctCode &&
-    new Date(currentDate) <= new Date(expirationDate)
-  );
+    return (
+        enteredCode === correctCode &&
+        new Date(currentDate) <= new Date(expirationDate)
+    );
 }
 
 function unluckyDays(year) {
-  let count = 0;
-  for (let i = 0; i < 12; i++) {
-    if (new Date(year, i, 13).getDay() == 5) {
-      count++;
+    let count = 0;
+    for (let i = 0; i < 12; i++) {
+        if (new Date(year, i, 13).getDay() == 5) {
+            count++;
+        }
     }
-  }
 
-  return count;
+    return count;
 }
 
 function myLanguages(results) {
-  let arr = Object.entries(results);
-  let result = [];
+    let arr = Object.entries(results);
+    let result = [];
 
-  arr.sort(function (a, b) {
-    return a[1] < b[1] ? 1 : -1;
-  });
+    arr.sort(function (a, b) {
+        return a[1] < b[1] ? 1 : -1;
+    });
 
-  console.log(arr);
-  arr.forEach((key) => {
-    key[1] >= 60 ? result.push(key[0]) : false;
-  });
-  return result;
+    console.log(arr);
+    arr.forEach((key) => {
+        key[1] >= 60 ? result.push(key[0]) : false;
+    });
+    return result;
 }
 
 // let validWord = function (dictionary, word) {
@@ -99,50 +99,51 @@ function myLanguages(results) {
 // validWord(["ab", "a", "abc"], "codewars");
 
 function roundToNext5(n) {
-  return n <= 0 || n % 5 == 0 ? n - (n % 5) : n + (5 - (n % 5));
+    return n <= 0 || n % 5 == 0 ? n - (n % 5) : n + (5 - (n % 5));
 }
 
 function sortByBit(arr) {
-  arr.sort(function (a, b) {
-    if (
-      Array.from(a.toString(2)).reduce((sum, el) => sum + +el, 0) >
-      Array.from(b.toString(2)).reduce((sum, el) => sum + +el, 0)
-    ) {
-      return 1;
-    }
-    if (
-      Array.from(a.toString(2)).reduce((sum, el) => sum + +el, 0) <
-      Array.from(b.toString(2)).reduce((sum, el) => sum + +el, 0)
-    ) {
-      return -1;
-    }
-    return a.toString(2) - b.toString(2);
-  });
-  // console.log(arr);
+    arr.sort(function (a, b) {
+        if (
+            Array.from(a.toString(2)).reduce((sum, el) => sum + +el, 0) >
+            Array.from(b.toString(2)).reduce((sum, el) => sum + +el, 0)
+        ) {
+            return 1;
+        }
+        if (
+            Array.from(a.toString(2)).reduce((sum, el) => sum + +el, 0) <
+            Array.from(b.toString(2)).reduce((sum, el) => sum + +el, 0)
+        ) {
+            return -1;
+        }
+        return a.toString(2) - b.toString(2);
+    });
+    // console.log(arr);
 }
 
 let a = [3, 8, 3, 6, 5, 7, 9, 1];
 sortByBit(a);
 
 class Song {
-  constructor(title, artist) {
-    this.title = title;
-    this.artist = artist;
-    this.listeners = [];
-  }
-  howMany(arr) {
-    let result = [];
-    arr.forEach((e) => {
-      if (this.listeners.includes(e.toLowerCase())) {
-      } else {
-        this.listeners.push(e.toLowerCase());
-        result.push(e);
-      }
-    });
+    constructor(title, artist) {
+        this.title = title;
+        this.artist = artist;
+        this.listeners = [];
+    }
 
-    // console.log(result.length);
-    return result.length;
-  }
+    howMany(arr) {
+        let result = [];
+        arr.forEach((e) => {
+            if (this.listeners.includes(e.toLowerCase())) {
+            } else {
+                this.listeners.push(e.toLowerCase());
+                result.push(e);
+            }
+        });
+
+        // console.log(result.length);
+        return result.length;
+    }
 }
 
 let mountMoose = new Song("Mount Moose", "The Snazzy Moose");
@@ -150,44 +151,50 @@ mountMoose.howMany(["John", "joHN", "carl"]);
 mountMoose.howMany(["John", "Fred", "Bob", "Carl", "RyAn"]);
 
 function singleDigit(n) {
-  if (n < 10) {
-    return n;
-  }
-  let sum = Array.from(n.toString(2)).reduce((a, b) => a + +b, 0);
-  return sum < 10 ? sum : singleDigit(sum);
+    if (n < 10) {
+        return n;
+    }
+    let sum = Array.from(n.toString(2)).reduce((a, b) => a + +b, 0);
+    return sum < 10 ? sum : singleDigit(sum);
 }
 
 singleDigit(5);
 
 function faroCount(deckSize) {
-  let arr = [];
-  let firstHalf = [];
-  let secondHalf = [];
-  let shuffleDeck = [];
-  let count = 1;
+    let arr = [];
+    let firstHalf = [];
+    let secondHalf = [];
+    let shuffleDeck = [];
+    let count = 1;
 
-  for (let i = 0; i < deckSize; i++) {
-    arr.push(i);
-    i < deckSize / 2 ? firstHalf.push(i) : secondHalf.push(i);
-  }
-
-  for (let j = 0; j < deckSize / 2; j++) {
-    shuffleDeck.push(firstHalf[j]);
-    shuffleDeck.push(secondHalf[j]);
-  }
-
-  while (shuffleDeck.join("") != arr.join("")) {
-    firstHalf = shuffleDeck.slice(0, deckSize / 2);
-    secondHalf = shuffleDeck.slice(deckSize / 2);
-    shuffleDeck = [];
-    for (let j = 0; j < deckSize / 2; j++) {
-      shuffleDeck.push(firstHalf[j]);
-      shuffleDeck.push(secondHalf[j]);
+    for (let i = 0; i < deckSize; i++) {
+        arr.push(i);
+        i < deckSize / 2 ? firstHalf.push(i) : secondHalf.push(i);
     }
-    count++;
-  }
 
-  console.log(count);
+    for (let j = 0; j < deckSize / 2; j++) {
+        shuffleDeck.push(firstHalf[j]);
+        shuffleDeck.push(secondHalf[j]);
+    }
+
+    while (shuffleDeck.join("") != arr.join("")) {
+        firstHalf = shuffleDeck.slice(0, deckSize / 2);
+        secondHalf = shuffleDeck.slice(deckSize / 2);
+        shuffleDeck = [];
+        for (let j = 0; j < deckSize / 2; j++) {
+            shuffleDeck.push(firstHalf[j]);
+            shuffleDeck.push(secondHalf[j]);
+        }
+        count++;
+    }
+
+    console.log(count);
 }
 
-faroCount(30);
+// faroCount(30);
+
+function bump(x){
+    return x.match(/n/g) == null || x.match(/n/g).length <= 15 ? "Woohoo!" : "Car Dead";
+}
+
+bump("___");

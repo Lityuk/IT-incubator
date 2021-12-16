d87dea135bf0e5361c0f876dd2ebf44056b92695
Lityuk <lityuk@gmail.com>
2021-12-16 20:12:30 +0300

    add 02 lesson files

---
+++ vanila-project/src/02/02.ts
@@ -0,0 +1,49 @@
+type CityType = {
+  title: string;
+  countryTitle: string;
+};
+
+type AddressType = {
+  streetTitle: string;
+  city: CityType;
+};
+
+type TechType = {
+  id: number;
+  title: string;
+};
+
+type studentType = {
+  id: number;
+  name: string;
+  age: number;
+  isActive: boolean;
+  address: AddressType;
+  technologies: Array<TechType>;
+};
+
+const student: studentType = {
+  id: 1,
+  name: "Vova",
+  age: 21,
+  isActive: false,
+  address: {
+    streetTitle: "Vostochnaya",
+    city: {
+      title: "Minsk",
+      countryTitle: "Belarus",
+    },
+  },
+
+  technologies: [
+    { id: 1, title: "HTML" },
+    { id: 2, title: "CSS" },
+    { id: 3, title: "React" },
+  ],
+};
+
+console.log(student.age);
+console.log(student.name);
+console.log(student.isActive);
+console.log(student.address.city.title);
+console.log(student.technologies[2].title);

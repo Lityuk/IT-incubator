5f50eae19f50d3caaa2d894820fc8e684c2099f4
Lityuk <lityuk@gmail.com>
2021-12-28 10:28:59 +0300

    add css modules

--- React/typescript-app/src/components/Navbar.tsx
+++ React/typescript-app/src/components/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav className="nav">
       <ul>
-        <li>Profile</li>
-        <li>Messages</li>
-        <li>News</li>
-        <li>Music</li>
-        <li>Settings</li>
+        <li className='item'>Profile</li>
+        <li className='item'>Messages</li>
+        <li className='item'>News</li>
+        <li className='item'>Music</li>
+        <li className='item'>Settings</li>
       </ul>
     </nav>
   );

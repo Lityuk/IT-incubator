ffc4e901ab459a89e33fca4f770c29cfdee5e6de
Lityuk <lityuk@gmail.com>
2022-01-04 10:39:02 +0300

    finish 3 task

--- ignat-dz/src/p2-homeworks/h3/GreetingContainer.tsx
+++ ignat-dz/src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
-    users: string // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,25 +13,40 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+    const [disabled, setDisabled] = useState<boolean>(false) // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
-    }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
+        setDisabled(false)
     }
 
-    const totalUsers = 0 // need to fix
+    const addUser = () => { // need to fix
+        const nameTrim = name.trim();
+
+        if (nameTrim.length !== 0) {
+            setError('')
+            addUserCallback(nameTrim)
+            alert(`Hello ${nameTrim}`)
+            setName('')
+        } else {
+            setError('The name is incorrect!')
+            setDisabled(true);
+        }
+    }
+
+    const totalUsers = users.length // need to fix
 
     return (
         <Greeting
             name={name}
             setNameCallback={setNameCallback}
             addUser={addUser}
-            error={error}
             totalUsers={totalUsers}
+            error={error}
+            disabled={disabled}
         />
     )
 }

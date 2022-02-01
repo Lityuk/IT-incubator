725e39af1ab8126f1e88506012fba4075929dd3e
Lityuk <lityuk@gmail.com>
2022-02-01 23:00:07 +0300

    feat: solve Regexp 14-15 tasks

--- freecodecamp/regExp.js
+++ freecodecamp/regExp.js
@@ -51,3 +51,9 @@ let result = difficultSpelling.match(myRegex);
 let chewieRegex = /Aa*/; // Change this line
 // Only change code above this line
 let result = chewieQuote.match(chewieRegex);
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
+
+let reCriminals = /C+/; // Change this line

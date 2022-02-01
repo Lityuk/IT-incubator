e4b97db8bc12246ddd8253e57d014a5143b4de6c
Lityuk <lityuk@gmail.com>
2022-02-01 23:49:59 +0300

    feat: solve 2 kata  CDW  7 kyu

--- Codewars/script2.js
+++ Codewars/script2.js
@@ -1,17 +1,32 @@
 var list1 = [
-    { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
-    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+    { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+    { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
 ];
 
 function getAverageAge(list) {
 
     return Math.round((list.reduce((a, b) => {
         return a + b.age;
-    },0)) / list.length)
+    }, 0)) / list.length)
 }
 
-console.log(getAverageAge(list1))
+// console.log(getAverageAge(list1))
 
 
-const remainder = (D, d) => D - Math.floor(D/d)*d
+const remainder = (D, d) => D - Math.floor(D / d) * d
+
+
+function isRubyComing(list) {
+
+    return list.map(i => i.language === 'Ruby').find(el => el === true) ? true : false
+
+}
+
+// console.log(isRubyComing(list1))
+
+function getFirstPython(list) {
+    let result = list.find(i => (i.language === 'Python'));
+    return  result === undefined ?`There will be no Python developers` : `${result.firstName}, ${result.country}`
+}
 

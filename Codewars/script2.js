457f55cad5ab91e9cf0422f1e1e8e814996c8bd7
Lityuk <lityuk@gmail.com>
2022-03-02 00:18:00 +0300

    solve cdw

--- Codewars/script2.js
+++ Codewars/script2.js
@@ -1,32 +1,94 @@
 var list1 = [
-    { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
-    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
-    { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+  {
+    firstName: null,
+    lastName: "I.",
+    country: "Argentina",
+    continent: "Americas",
+    age: 35,
+    language: "Java",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "X.",
+    country: "Croatia",
+    continent: "Europe",
+    age: 35,
+    language: null,
+  },
+  {
+    firstName: "Madison",
+    lastName: "U.",
+    country: "United States",
+    continent: "Americas",
+    age: 32,
+    language: "Ruby",
+  },
 ];
 
 function getAverageAge(list) {
-
-    return Math.round((list.reduce((a, b) => {
-        return a + b.age;
-    }, 0)) / list.length)
+  return Math.round(
+    list.reduce((a, b) => {
+      return a + b.age;
+    }, 0) / list.length
+  );
 }
 
 // console.log(getAverageAge(list1))
 
-
-const remainder = (D, d) => D - Math.floor(D / d) * d
-
+const remainder = (D, d) => D - Math.floor(D / d) * d;
 
 function isRubyComing(list) {
-
-    return list.map(i => i.language === 'Ruby').find(el => el === true) ? true : false
-
+  return list.map((i) => i.language === "Ruby").find((el) => el === true)
+    ? true
+    : false;
 }
 
 // console.log(isRubyComing(list1))
 
 function getFirstPython(list) {
-    let result = list.find(i => (i.language === 'Python'));
-    return  result === undefined ?`There will be no Python developers` : `${result.firstName}, ${result.country}`
+  let result = list.find((i) => i.language === "Python");
+  return result === undefined
+    ? `There will be no Python developers`
+    : `${result.firstName}, ${result.country}`;
 }
 
+list1 = [
+  {
+    firstName: null,
+    lastName: "I.",
+    country: "Argentina",
+    continent: "Americas",
+    age: 35,
+    language: "Java",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "X.",
+    country: "Croatia",
+    continent: "Europe",
+    age: 35,
+    language: null,
+  },
+  {
+    firstName: "Madison",
+    lastName: "U.",
+    country: "United States",
+    continent: "Americas",
+    age: 32,
+    language: "Ruby",
+  },
+];
+
+function askForMissingDetails(list) {
+  return  result = list.map((i) => {
+    for (let val in i) {
+      if (i[val] === null) {
+        i.question = `Hi, could you please provide your ${val}.`;
+        return i;
+      }
+    }
+  }).filter(j => j != undefined);
+
+}
+
+console.log(askForMissingDetails(list1)[0]);
